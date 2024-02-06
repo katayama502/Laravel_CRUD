7ed2b5329a4785759b9324ff11fe1a0ae14e4808
katayama hayato <>
2024-02-06 16:20:09 +0900

    デベロップにpush

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <!-- develop branch -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>会員一覧</title>
